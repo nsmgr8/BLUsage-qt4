b684ff4e893f8a448a7ebb931e5ca6cd92b82942
M. Nasimul Haque <nasim.haque@gmail.com>
2011-06-29 18:24:52 +0600

    layout main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,28 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QMenu *menu = new QMenu(QString("File"), this);
+    menu->addAction(ui->actionAccount);
+    menu->addAction(ui->actionUpdate);
+    menu->addSeparator();
+    menu->addAction(ui->actionQuit);
+
+    ui->menuBar->addMenu(menu);
+
+    this->connect(ui->actionAccount, SIGNAL(triggered()), SLOT(showAccountEditor()));
+    this->connect(ui->actionUpdate, SIGNAL(triggered()), SLOT(updateUsage()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::showAccountEditor() {
+    qDebug("Open the account editor");
+}
+
+void MainWindow::updateUsage() {
+    qDebug("Update usage");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,10 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void showAccountEditor();
+    void updateUsage();
+
 private:
     Ui::MainWindow *ui;
 };
