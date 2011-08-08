47c87fb037ea0e86672ca7d8731271d8fe4b2824
Nasimul Haque <nasim.haque@gmail.com>
2011-08-08 17:22:14 +0600

    added a simple install script for ubuntu

---
+++ install_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This software requires PySide, which will be installed"
+echo "from a PPA repository."
+echo "Please enter your password to install all required libs."
+sudo python install_ubuntu.py
+
